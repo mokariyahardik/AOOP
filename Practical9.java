7439040ecc479c4123c806be4de84a27a4a123fa
mokariyahardik <162258355+mokariyahardik@users.noreply.github.com>
2025-04-22 12:07:13 +0530

    Create Practical9.java

---
+++ Practical9.java
@@ -0,0 +1,16 @@
+class Hardik {
+   
+    static int count = 0;
+
+  
+    static void incrementCount() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+             incrementCount();
+      
+
+              System.out.println("Count: " + count);
+    }
+}
